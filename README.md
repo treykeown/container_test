55d63cf8dc02d2c70c899214913e4d40a742a4c5
treykeown <2755914+treykeown@users.noreply.github.com>
2023-06-27 14:39:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# container_bench
+
+Inspired by this: https://janhendrikewers.uk/pydantic_vs_protobuf_vs_namedtuple_vs_dataclasses.html
+
+![Results graph](results.png)
+
+Results:
+
+```
+NTCoords(coords=[NTCoord(x=80, y=82, z=40, heading=19), NTCoord(x=36, y=85, z=91, heading=23), NTCoord(x=57, y=16, z=28, heading=47), NTCoord(x=14, y=9, z=11, heading=24), NTCoord(x=24, y=62, z=7, heading=84), NTCoord(x=83, y=45, z=66, heading=45), NTCoord(x=41, y=56, z=96, heading=95), NTCoord(x=6, y=69, z=94, heading=20), NTCoord(x=65, y=83, z=17, heading=61), NTCoord(x=44, y=11, z=69, heading=2), NTCoord(x=4, y=47, z=92, heading=75), NTCoord(x=56, y=37, z=3, heading=82), NTCoord(x=52, y=13, z=34, heading=78), NTCoord(x=16, y=71, z=35, heading=87), NTCoord(x=59, y=1, z=17, heading=79)])
+NTCoords(coords=[NTCoord(x=80, y=82, z=40, heading=19), NTCoord(x=36, y=85, z=91, heading=23), NTCoord(x=57, y=16, z=28, heading=47), NTCoord(x=14, y=9, z=11, heading=24), NTCoord(x=24, y=62, z=7, heading=84), NTCoord(x=83, y=45, z=66, heading=45), NTCoord(x=41, y=56, z=96, heading=95), NTCoord(x=6, y=69, z=94, heading=20), NTCoord(x=65, y=83, z=17, heading=61), NTCoord(x=44, y=11, z=69, heading=2), NTCoord(x=4, y=47, z=92, heading=75), NTCoord(x=56, y=37, z=3, heading=82), NTCoord(x=52, y=13, z=34, heading=78), NTCoord(x=16, y=71, z=35, heading=87), NTCoord(x=59, y=1, z=17, heading=79)])
+namedtuple - create: 0.747 total, 1.49e-05 avg
+namedtuple - serialize: 0.336 total, 6.73e-06 avg
+namedtuple - deserialize: 0.288 total, 5.76e-06 avg
+
+DCCoords(coords=[DCCoord(x=80, y=82, z=40, heading=19), DCCoord(x=36, y=85, z=91, heading=23), DCCoord(x=57, y=16, z=28, heading=47), DCCoord(x=14, y=9, z=11, heading=24), DCCoord(x=24, y=62, z=7, heading=84), DCCoord(x=83, y=45, z=66, heading=45), DCCoord(x=41, y=56, z=96, heading=95), DCCoord(x=6, y=69, z=94, heading=20), DCCoord(x=65, y=83, z=17, heading=61), DCCoord(x=44, y=11, z=69, heading=2), DCCoord(x=4, y=47, z=92, heading=75), DCCoord(x=56, y=37, z=3, heading=82), DCCoord(x=52, y=13, z=34, heading=78), DCCoord(x=16, y=71, z=35, heading=87), DCCoord(x=59, y=1, z=17, heading=79)])
+DCCoords(coords=[DCCoord(x=80, y=82, z=40, heading=19), DCCoord(x=36, y=85, z=91, heading=23), DCCoord(x=57, y=16, z=28, heading=47), DCCoord(x=14, y=9, z=11, heading=24), DCCoord(x=24, y=62, z=7, heading=84), DCCoord(x=83, y=45, z=66, heading=45), DCCoord(x=41, y=56, z=96, heading=95), DCCoord(x=6, y=69, z=94, heading=20), DCCoord(x=65, y=83, z=17, heading=61), DCCoord(x=44, y=11, z=69, heading=2), DCCoord(x=4, y=47, z=92, heading=75), DCCoord(x=56, y=37, z=3, heading=82), DCCoord(x=52, y=13, z=34, heading=78), DCCoord(x=16, y=71, z=35, heading=87), DCCoord(x=59, y=1, z=17, heading=79)])
+dataclass - create: 0.737 total, 1.47e-05 avg
+dataclass - serialize: 2.62 total, 5.24e-05 avg
+dataclass - deserialize: 0.465 total, 9.3e-06 avg
+
+PDCoords(coords=[PDCoord(x=80, y=82, z=40, heading=19), PDCoord(x=36, y=85, z=91, heading=23), PDCoord(x=57, y=16, z=28, heading=47), PDCoord(x=14, y=9, z=11, heading=24), PDCoord(x=24, y=62, z=7, heading=84), PDCoord(x=83, y=45, z=66, heading=45), PDCoord(x=41, y=56, z=96, heading=95), PDCoord(x=6, y=69, z=94, heading=20), PDCoord(x=65, y=83, z=17, heading=61), PDCoord(x=44, y=11, z=69, heading=2), PDCoord(x=4, y=47, z=92, heading=75), PDCoord(x=56, y=37, z=3, heading=82), PDCoord(x=52, y=13, z=34, heading=78), PDCoord(x=16, y=71, z=35, heading=87), PDCoord(x=59, y=1, z=17, heading=79)])
+coords=[PDCoord(x=80, y=82, z=40, heading=19), PDCoord(x=36, y=85, z=91, heading=23), PDCoord(x=57, y=16, z=28, heading=47), PDCoord(x=14, y=9, z=11, heading=24), PDCoord(x=24, y=62, z=7, heading=84), PDCoord(x=83, y=45, z=66, heading=45), PDCoord(x=41, y=56, z=96, heading=95), PDCoord(x=6, y=69, z=94, heading=20), PDCoord(x=65, y=83, z=17, heading=61), PDCoord(x=44, y=11, z=69, heading=2), PDCoord(x=4, y=47, z=92, heading=75), PDCoord(x=56, y=37, z=3, heading=82), PDCoord(x=52, y=13, z=34, heading=78), PDCoord(x=16, y=71, z=35, heading=87), PDCoord(x=59, y=1, z=17, heading=79)]
+pydantic-v1 - create: 5.06 total, 0.000101 avg
+pydantic-v1 - serialize: 2.76 total, 5.53e-05 avg
+pydantic-v1 - deserialize: 4.76 total, 9.52e-05 avg
+```
+
+Pydantic V2 results: `create=1.56, serialize=0.531, deserialize=0.594`
---
+++ container_bench/__main__.py
@@ -0,0 +1,193 @@
+"""benchmark various types of object containers"""
+
+import dataclasses
+import json
+import timeit
+from collections import namedtuple, defaultdict
+from typing import Callable
+
+import arguably
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pydantic
+
+RUNS = 1000
+data: tuple[tuple[int], tuple[int], tuple[int], tuple[int]] | None = None
+
+results: dict[str, list[float]] = defaultdict(list)
+
+
+def inject_pydantic_v2_results():
+    raise Exception("Pydantic V2 numbers will need to be updated if you run on your machine!!!")
+    # To get results for V2: `poetry remove pydantic` && `poetry add --allow-prereleases pydantic`
+    results["pydantic-v2"].extend([1.56, 0.531, 0.594])
+
+
+def data_to_items(t, f, g, h, i):
+    return [t(x=fi.item(), y=gi.item(), z=hi.item(), heading=ii.item()) for fi, gi, hi, ii in zip(f, g, h, i)]
+
+
+def time_run(group: str, trial: str, func: Callable) -> float:
+    duration = timeit.Timer(func).timeit(number=RUNS)
+    avg_duration = duration / RUNS
+    print(f"{group} - {trial}: {duration:.3} total, {avg_duration:.3} avg")
+    results[group].append(duration)
+    return duration
+
+
+########################################################################################################################
+# dataclasses
+
+@dataclasses.dataclass
+class DCCoord:
+    x: int
+    y: int
+    z: int
+    heading: int
+
+
+@dataclasses.dataclass
+class DCCoords:
+    coords: list[DCCoord]
+
+
+class EnhancedJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        return super().default(o)
+
+
+@arguably.command
+def dataclass_():
+    """benchmark dataclasses"""
+    global data
+    if data is None:
+        data = np.random.randint(0, 100, size=(4, 15), dtype=int)
+    xs, ys, zs, headings = data
+
+    create = lambda: DCCoords(coords=data_to_items(DCCoord, xs, ys, zs, headings))
+    serialize = lambda: json.dumps(coords, separators=(",", ":"), cls=EnhancedJSONEncoder)
+    deserialize = lambda: DCCoords(coords=[DCCoord(**x) for x in json.loads(serialized)["coords"]])
+
+    coords = create()
+    serialized = serialize()
+    print(coords)
+    print(deserialize())
+
+    time_run("dataclass", "create", create)
+    time_run("dataclass", "serialize", serialize)
+    time_run("dataclass", "deserialize", deserialize)
+    print()
+
+
+########################################################################################################################
+# pydantic
+
+@pydantic.dataclasses.dataclass
+class PDCoord:
+    x: int
+    y: int
+    z: int
+    heading: int
+
+
+@pydantic.dataclasses.dataclass
+class PDCoords:
+    coords: list[PDCoord]
+
+
+@arguably.command
+def pydantic_():
+    """benchmark pydantic (can be v1 or v2)"""
+    global data
+    if data is None:
+        data = np.random.randint(0, 100, size=(4, 15), dtype=int)
+    xs, ys, zs, headings = data
+
+    create = lambda: PDCoords(coords=data_to_items(PDCoord, xs, ys, zs, headings))
+    if hasattr(pydantic, "RootModel"):
+        group = "pydantic-v2"
+        serialize = lambda: pydantic.RootModel[PDCoords](coords).model_dump_json()
+        deserialize = lambda: pydantic.RootModel[PDCoords].model_validate_json(serialized)
+    else:
+        inject_pydantic_v2_results()
+        from pydantic.json import pydantic_encoder
+        group = "pydantic-v1"
+        serialize = lambda: json.dumps(coords, default=pydantic_encoder)
+        deserialize = lambda: PDCoords.__pydantic_model__.parse_raw(serialized)
+
+    coords = create()
+    serialized = serialize()
+    print(coords)
+    print(deserialize())
+
+    time_run(group, "create", create)
+    time_run(group, "serialize", serialize)
+    time_run(group, "deserialize", deserialize)
+    print()
+
+
+########################################################################################################################
+# namedtuple
+
+NTCoord = namedtuple("NTCoord", "x y z heading")
+NTCoords = namedtuple("NTCoords", "coords")
+
+
+@arguably.command
+def namedtuple_():
+    """benchmark namedtuple"""
+    global data
+    if data is None:
+        data = np.random.randint(0, 100, size=(4, 15), dtype=int)
+    xs, ys, zs, headings = data
+
+    create = lambda: NTCoords(coords=data_to_items(NTCoord, xs, ys, zs, headings))
+    serialize = lambda: json.dumps(coords._asdict(), separators=(",", ":"))
+    deserialize = lambda: NTCoords(coords=[NTCoord(*x) for x in json.loads(serialized)["coords"]])
+
+    coords = create()
+    serialized = serialize()
+    print(coords)
+    print(deserialize())
+
+    time_run("namedtuple", "create", create)
+    time_run("namedtuple", "serialize", serialize)
+    time_run("namedtuple", "deserialize", deserialize)
+    print()
+
+
+########################################################################################################################
+# all
+
+@arguably.command
+def __root__(*, runs: int = 50000):
+    global RUNS
+    RUNS = runs
+
+    if arguably.is_target:
+        namedtuple_()
+        dataclass_()
+        pydantic_()
+
+
+if __name__ == "__main__":
+    arguably.run()
+
+    df = pd.DataFrame(
+        [[group, *data] for group, data in results.items()],
+        columns=["Implementation", "create", "serialize", "deserialize"]
+    )
+
+    df.plot(
+        x="Implementation",
+        kind="bar",
+        stacked=False,
+        title="Test results",
+        rot=0,
+        ylabel=f"Seconds for {RUNS} iters"
+    )
+
+    plt.show()
